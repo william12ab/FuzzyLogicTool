765d9a877f7be165a4c65683185e41fa95991f8f
william12ab <58400045+william12ab@users.noreply.github.com>
2024-02-05 16:40:53 +0000

    added smooth functions

--- FuzzyLogicTool/GraphFunctionCalculator.cpp
+++ FuzzyLogicTool/GraphFunctionCalculator.cpp
@@ -77,3 +77,42 @@ float GraphFunctionCalculator::TrapezoidalFunction(const float& a, const float& 
 	}
 	return return_value;
 }
+
+float SmoothZFunction(const float& a, const float& b, const float& x) {
+	float return_value = 0.0f;
+	if (x<=a){
+		return_value = 1.0f;
+	}
+	if (a<=x && x<= (a+b)/(2.f)){
+		float brackets = (x - a) / (b - a);
+		brackets *= brackets;
+		brackets *= 2.0f;
+		return_value = 1.0f - brackets;
+	}
+	if (((a+b)/2.f) <=x && x<=b){
+		return_value = 2.f * ((x - b) / (b - a));
+	}
+	if (x >= b) {
+		return_value = 0.0f;
+	}
+	return return_value;
+}
+float SmoothSFunction(const float& a, const float& b, const float& x) {
+	float return_value = 0.0f;
+	if (x <= a) {
+		return_value = 0.0f;
+	}
+	if (a <= x && x <= (a + b) / (2.f)) {
+		return_value = 2.f * ((x - b) / (b - a));
+	}
+	if (((a + b) / 2.f) <= x && x <= b) {
+		float brackets = (x - a) / (b - a);
+		brackets *= brackets;
+		brackets *= 2.0f;
+		return_value = 1.0f - brackets;
+	}
+	if (x >= b) {
+		return_value = 1.0f;
+	}
+	return return_value;
+}
--- FuzzyLogicTool/GraphFunctionCalculator.h
+++ FuzzyLogicTool/GraphFunctionCalculator.h
@@ -5,6 +5,8 @@ public:
 	float TriangularFunction(const float& min, const float& max, const float& input_value);
 
 	float ZFunction(const float& min, const float& max, const float& input_value);
+	float SmoothZFunction(const float& min, const float& max, const float& input_value);
+	float SmoothSFunction(const float& min, const float& max, const float& input_value);
 	float SFunction(const float& min, const float& max, const float& input_value);
 	float TrapezoidalFunction(const float& min, const float& max, const float& input_value);
 };
