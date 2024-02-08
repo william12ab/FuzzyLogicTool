16255941be94dc71bfcca2d81dd27f8421abb5d4
william12ab <58400045+william12ab@users.noreply.github.com>
2024-02-08 16:03:25 +0000

    getting points

--- FuzzyLogicTool/DefuzzyCalculator.cpp
+++ FuzzyLogicTool/DefuzzyCalculator.cpp
@@ -2,40 +2,48 @@
 
 
 void DefuzzyCalculator::FindPoints(Operation new_operation) {
-	sf::Vector2f first_point = sf::Vector2f(new_operation.GetRuleVector()[0].GetConsequenceVector()[0].GetMin(),0);
+	sf::Vector2f first_point = sf::Vector2f(new_operation.GetRuleVector()[0].GetConsequenceVector()[0].GetMin(), 0);
 	polygon_points.emplace_back(first_point);
 	//point one
-	
+
 	sf::Vector2f peak = FindPeak(new_operation.GetRuleVector()[0].GetConsequenceVector()[0].GetMax(), first_point.x);
-	
-	polygon_points.emplace_back(FindEdgePoint(first_point, peak, new_operation.GetRuleVector()[0].GetOperatorValue(),0));
+
+	polygon_points.emplace_back(FindEdgePoint(first_point, peak, new_operation.GetRuleVector()[0].GetOperatorValue(), 0));
 	//point two
 
-	if (new_operation.GetRuleVector().size()>0){
-		auto point_three=FindEdgePoint(first_point, peak, new_operation.GetRuleVector()[0].GetOperatorValue(), 1);
-		sf::Vector2f point_five = sf::Vector2f(new_operation.GetRuleVector()[1].GetConsequenceVector()[0].GetMin(), 0);
-		peak = FindPeak(new_operation.GetRuleVector()[1].GetConsequenceVector()[0].GetMax(), point_three.x);
-		//point six
+	sf::Vector2f temp_last,temp_last2;
 
-		if (CheckForIntersection(polygon_points[1], point_three, point_five, peak)){
+	if (new_operation.GetRuleVector().size() > 0) {
+		int index = 1;
+		if (new_operation.GetRuleVector().size() > 1) {
+			index = new_operation.GetRuleVector().size() - 1;
 		}
-		else {
-			polygon_points.emplace_back(point_three);
-			sf::Vector2f point_four = sf::Vector2f(new_operation.GetRuleVector()[0].GetConsequenceVector()[0].GetMax(), 0);
-			CheckForIntersection(point_three, point_four, point_five, peak);
+		for (size_t i = 0; i < index; i++) {
+			auto point_three = FindEdgePoint(first_point, peak, new_operation.GetRuleVector()[i].GetOperatorValue(), 1);
+			sf::Vector2f point_five = sf::Vector2f(new_operation.GetRuleVector()[i + 1].GetConsequenceVector()[0].GetMin(), 0);
+			peak = FindPeak(new_operation.GetRuleVector()[i + 1].GetConsequenceVector()[0].GetMax(), point_three.x);
+			//point six
+
+			if (CheckForIntersection(polygon_points[i + 1], point_three, point_five, peak)) {
+			}
+			else {
+				polygon_points.emplace_back(point_three);
+				sf::Vector2f point_four = sf::Vector2f(new_operation.GetRuleVector()[i].GetConsequenceVector()[0].GetMax(), 0);
+				CheckForIntersection(point_three, point_four, point_five, peak);
+			}
+			auto point_six = FindEdgePoint(point_five, peak, new_operation.GetRuleVector()[i + 1].GetOperatorValue(), 0);
+			polygon_points.emplace_back(point_six);
+			first_point = point_five;
+			//at here need to change first point and peak to new values
+			temp_last = point_five;
+			temp_last2 = peak;
 		}
-		polygon_points.emplace_back(peak);
+		//find p7 and p8
 	}
-
-	//add p1
-	//add p2
-	//	if rules size>0
-	//		if (p2,p3) intersect with (p5,p6) - Add this point
-	//		else add p3
-	//			in this else find where (p3,p4) intersect with (p5,p6)
-	//			add this point
-	//	add p6 	
-
+	auto point_seven = FindEdgePoint(temp_last, temp_last2, new_operation.GetRuleVector()[new_operation.GetRuleVector().size() - 1].GetOperatorValue(), 1);
+	auto point_eight = sf::Vector2f(new_operation.GetRuleVector()[new_operation.GetRuleVector().size() - 1].GetOperatorValue(), 0);
+	polygon_points.emplace_back(point_seven);
+	polygon_points.emplace_back(point_eight);
 }
 sf::Vector2f DefuzzyCalculator::FindPeak(const float& max_, const float& x_coord) {
 	auto max_p = max_;
@@ -47,11 +55,11 @@ sf::Vector2f DefuzzyCalculator::FindPeak(const float& max_, const float& x_coord
 
 sf::Vector2f DefuzzyCalculator::FindEdgePoint(const sf::Vector2f& first_point, const sf::Vector2f& peak_point, const float& implication_value, const int& multplier_) {
 	//y-y1=m(x-x1) where (x,y) y=implication from r1
-	float m = (peak_point.y-first_point.y) / (peak_point.x-first_point.x);
-	if (multplier_==1){
+	float m = (peak_point.y - first_point.y) / (peak_point.x - first_point.x);
+	if (multplier_ == 1) {
 		m *= -1;
 	}
-	float x = (implication_value-peak_point.y+(m* peak_point.x)) / m;
+	float x = (implication_value - peak_point.y + (m * peak_point.x)) / m;
 	sf::Vector2f point = sf::Vector2f(x, implication_value);
 	polygon_points.emplace_back(point);
 	return point;
@@ -64,7 +72,7 @@ bool DefuzzyCalculator::CheckForIntersection(const sf::Vector2f& point_one, cons
 	bool is_intersect;
 	float m1 = (point_two.y - point_one.y) / (point_two.x - point_one.x);
 	float m2 = (point_four.y - point_three.y) / (point_four.x - point_three.x);
-	if (m1 != m2){
+	if (m1 != m2) {
 		float b1 = point_one.y - (m1 * (point_one.x));
 		float b2 = point_three.y - (m2 * (point_three.x));
 
